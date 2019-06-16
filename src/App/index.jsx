df39303758bfad2f00d8d83138ed2a1895303190
Aleksander Nikolyuk <anikolyk@gmail.com>
2019-06-16 22:26:27 +0300

    actors list

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import React from 'react';
 import logo from './logo.svg';
 import styles from './styles.module.scss';
 
-function App() {
-  return (
-    <div className={styles['App']}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles['App-link']}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const actors = ['Jon Snow', 'Rob Stark', 'Sam Tarly'];
+
+class App extends Component {
+  state = {
+    value: '',
+    actors,
+  }
+
+  // componentDidMount() {
+  //   this.setState((state) => {
+  //     state.actors.map(item => (
+  //       <li key={item}>{item}</li>
+  //     ));
+  //   });
+  // }
+
+  changeValue = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  addActors = () => {
+    this.setState((state) => {
+      const actors = state.actors.concat(state.value);
+      return {
+        actors,
+        value: '',
+      };
+    });
+  };
+
+  clearList = () => {
+    this.setState({
+      actors: [],
+    });
+  };
+
+  resetList = () => {
+    this.setState({ actors });
+  };
+
+  render() {
+    const {
+      addActors, clearList, resetList, changeValue,
+    } = this;
+    const listActors = this.state.actors.map(item => (
+      <li key={item}>{item}</li>
+    ));
+
+    return (
+      <div className={styles.App}>
+        <header className={styles['App-header']}>
+          <img src={logo} className={styles['App-logo']} alt="logo" />
+          <div className={styles['App-buttons']}>
+            <ul>{listActors}</ul>
+          </div>
+          <div className={styles['App-buttons']}>
+            <input type="text" placeholder="Add name" value={this.state.value} onChange={changeValue} />
+            <button className={styles.green} type="button" onClick={addActors}>ADD</button>
+          </div>
+          <div className={styles['App-buttons']}>
+            <button type="button" onClick={clearList}>CLEAR</button>
+            <button className={styles.red} type="button" onClick={resetList}>RESET</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
